56b4e5d60c2c60b1ac938fc34fc41324804c00c8
TAMURA IBUKI <ibu0414@yahoo.co.jp>
2025-05-08 01:16:24 +0900

    Initial Commit

---
+++ XSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XSampleApp
+//
+//  Created by 田村伊吹 on 2025/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
