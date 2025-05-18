b28cdfa5cb6f81874a8acdc5b2c5179aa4d3d449
TAMURA IBUKI <ibu0414@yahoo.co.jp>
2025-05-18 12:59:03 +0900

    HomeTableViewCell作成

---
+++ XSampleApp/HomeTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  HomeTableViewCell.swift
+//  XSampleApp
+//
+//  Created by 田村伊吹 on 2025/05/18.
+//
+
+import UIKit
+
+/// ホーム画面のセル
+class HomeTableViewCell: UICollectionViewCell {
+    
+    /// ユーザーイメージビュー
+    @IBOutlet private weak var usserImageView: UIImageView!
+    /// ユーザー各ラベル
+    @IBOutlet private weak var userNameLabel: UILabel!
+    /// 本文ラベル
+    @IBOutlet private weak var bodyLabel: UILabel!
+    
+    func configure(imageString: String, name: String, body: String) {
+        self.usserImageView.image = UIImage(named: imageString)
+        self.userNameLabel.text = name
+        self.bodyLabel.text = body
+    }
+}
