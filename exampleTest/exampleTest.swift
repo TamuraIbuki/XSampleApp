530bfde3f2e304777d5858ac436c399cdc247058
高橋智一 <takahashitomokazu5@gmail.com>
2025-06-03 20:40:45 +0900

    コンフリクトテスト

--- exampleTest/exampleTest.swift
+++ exampleTest/exampleTest.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import RealmSwift
 @testable import XSampleApp
 
 final class exampleTest: XCTestCase {
@@ -18,10 +19,25 @@ final class exampleTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
+    /// bodyCount関数に入れた文字列が空じゃなかった場合、trueを返すかをテスト
     func testBodyCount() throws {
         let vc = PostEditViewController()
-        let bodyLimit = String(repeating: "あ", count: 140)
-        XCTAssertTrue(vc.bodyCount(bodyLimit))
+        let result = vc.bodyCount("")
+        XCTAssertTrue(result)
+    }
+    
+    /// bodyCount関数に入れた文字列が140文字以下だった場合、trueを返すかをテスト
+    func testBodyCount2() throws {
+        let vc = PostEditViewController()
+        let result = vc.bodyCount("小さな一歩でも、毎日続ければ大きな成果につながる。焦らず、コツコツ進もう。")
+        XCTAssertTrue(result)
+    }
+    
+    /// bodyCount関数に入れた文字列が141文字以上だった場合、falseを返すかをテスト
+    func testBodyCount3() throws {
+        let vc = PostEditViewController()
+        let result = vc.bodyCount("毎日少しずつでも前に進むことが大切です。成果が見えない時期があっても、積み重ねは必ず自分の力になります。")
+        XCTAssertFalse(result)
     }
     
     func testExample() throws {
